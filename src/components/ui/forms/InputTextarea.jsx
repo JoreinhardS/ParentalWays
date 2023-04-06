623f547c2e7887d3be195b0aff1fa669e360128f
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-04-06 21:45:17 +0700

    refactor: add outline none

--- src/components/ui/forms/InputTextarea.jsx
+++ src/components/ui/forms/InputTextarea.jsx
@@ -4,7 +4,7 @@ export function InputTextarea({ className, rows = 4, ...rest }) {
   return (
     <textarea
       className={cn(
-        'block w-full text-base text-black-10 border-[#B6B6B6] rounded-lg',
+        'block w-full text-base text-black-10 border-[#B6B6B6] rounded-lgv outline-none',
         className
       )}
       rows={rows}
