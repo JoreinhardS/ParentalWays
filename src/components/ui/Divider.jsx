11bd8c6d576cb835b2cd906e71a046f3317e9b7d
AkmalSeptiyana <takaakmal10969@gmail.com>
2023-04-21 18:56:04 +0700

    refactor: border bottom divider

--- src/components/ui/Divider.jsx
+++ src/components/ui/Divider.jsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
 
 export function Divider({ className }) {
-  return <hr className={cn('border border-[#9B9B9B]', className)} />;
+  return <hr className={cn('border-b border-[#9B9B9B]', className)} />;
 }
